ddd480f67e2ae1219d47b12b7243503b188ca611
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-03 12:16:30 -0700

    added mouseenter and mouseleave events to intro image, added intro-img id to image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// Scaling Intro Image
+const scaleUp = document.getElementById("intro-img");
+scaleUp.addEventListener("mouseenter", e => {
+  scaleUp.style.transform = "scale(1.3)";
+  scaleUp.style.transition = "transform 0.5s";
+});
+const scaleDown = document.getElementById("intro-img");
+scaleDown.addEventListener("mouseleave", e => {
+  scaleDown.style.transform = "scale(1)";
+  scaleDown.style.transition = "transform 0.5s";
+});
